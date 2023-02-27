a9c79ac148786c02b2ba25f4ae9ba75590d48bdc
Khanh <khanhdaydung@gmail.com>
2023-02-27 22:00:32 +0700

    Add: Back end for Appointment table screen

---
+++ src/main/java/com/example/swd392_clinic_management/dal/AppointmentDAO.java
@@ -0,0 +1,200 @@
+package com.example.swd392_clinic_management.dal;
+
+import com.example.swd392_clinic_management.DTO.AppointmentDTO;
+import com.example.swd392_clinic_management.model.Appointment;
+import com.example.swd392_clinic_management.model.User;
+import com.example.swd392_clinic_management.util.DBUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AppointmentDAO extends DBUtil {
+    PreparedStatement ps;
+    ResultSet rs;
+    Connection connection = getConnection();
+
+    public List<Appointment> getAllAppointment() {
+        List<Appointment> list = new ArrayList<>();
+        String query = "select * from Appointment";
+        try {
+            ps = connection.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Appointment(
+                        rs.getInt("AppointId"),
+                        rs.getInt("patientId"),
+                        rs.getInt("doctorId"),
+                        rs.getTimestamp("time"),
+                        rs.getString("note"),
+                        rs.getInt("status")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public List<Appointment> getAppointmentByUserId(int userId) {
+        List<Appointment> list = new ArrayList<>();
+        String query = "select * from Appointment where patientId = ? or doctorId = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, userId);
+            ps.setInt(2, userId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Appointment(
+                        rs.getInt("AppointId"),
+                        rs.getInt("patientId"),
+                        rs.getInt("doctorId"),
+                        rs.getTimestamp("time"),
+                        rs.getString("note"),
+                        rs.getInt("status")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public List<Appointment> getAppointmentPaging(List<Appointment> list, int start, int end) {
+        List<Appointment> arr = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            arr.add(list.get(i));
+        }
+        return arr;
+    }
+
+    public List<AppointmentDTO> getAppointmentDTOPaging(List<AppointmentDTO> list, int start, int end) {
+        List<AppointmentDTO> arr = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            arr.add(list.get(i));
+        }
+        return arr;
+    }
+
+    public List<AppointmentDTO> getAllAppointmentDTO() {
+        List<AppointmentDTO> list = new ArrayList<>();
+        String query = "select AppointId, u1.fullname as 'patientName', u2.fullname as 'doctorName', a.time, note, a.status from Appointment a\n" +
+                "join Users u1 on u1.userid = a.patientId\n" +
+                "join Users u2 on u2.userid = a.doctorId";
+        try {
+            ps = connection.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new AppointmentDTO(
+                        rs.getInt("AppointId"),
+                        rs.getString("patientName"),
+                        rs.getString("doctorName"),
+                        rs.getTimestamp("time"),
+                        rs.getString("note"),
+                        rs.getInt("status")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public List<AppointmentDTO> getAppointmentDTOByUserId(int userId) {
+        List<AppointmentDTO> list = new ArrayList<>();
+        String query = "select AppointId, u1.fullname as 'patientName', u2.fullname as 'doctorName', a.time, note, a.status from Appointment a\n" +
+                "join Users u1 on u1.userid = a.patientId\n" +
+                "join Users u2 on u2.userid = a.doctorId\n" +
+                "where patientId = ? or doctorId = ?\n";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, userId);
+            ps.setInt(2, userId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new AppointmentDTO(
+                        rs.getInt("AppointId"),
+                        rs.getString("patientName"),
+                        rs.getString("doctorName"),
+                        rs.getTimestamp("time"),
+                        rs.getString("note"),
+                        rs.getInt("status")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public List<AppointmentDTO> searchAppointmentDTO(List<AppointmentDTO> list, String searchKey, int status) {
+        return list.stream()
+                .filter(a ->
+                        (status == -1 || a.getStatus() == status) &&
+                                (a.getPatientName().toLowerCase().contains(searchKey.toLowerCase()) ||
+                                        a.getDoctorName().toLowerCase().contains(searchKey.toLowerCase()))
+                ).collect(Collectors.toList());
+    }
+
+    public AppointmentDTO getByAppointmentId(int appointmentId) {
+        AppointmentDTO appointment = null;
+        String query = "select AppointId, u1.fullname as 'patientName', u2.fullname as 'doctorName', u2.position, a.time, note, a.status from Appointment a\n" +
+                "join Users u1 on u1.userid = a.patientId\n" +
+                "join Users u2 on u2.userid = a.doctorId\n" +
+                "where a.AppointId = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, appointmentId);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                appointment = new AppointmentDTO(
+                        rs.getInt("AppointId"),
+                        rs.getString("patientName"),
+                        rs.getString("doctorName"),
+                        rs.getTimestamp("time"),
+                        rs.getString("note"),
+                        rs.getInt("status")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return appointment;
+    }
+
+
+    public void updateAppointment(Appointment appointment) {
+        String query = "update Appointment set doctorId = ?, time = ?, note = ?, status = ? where AppointId = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, appointment.getDoctorId());
+            ps.setTimestamp(2, new Timestamp(appointment.getTime().getTime()));
+            ps.setString(3, appointment.getNote());
+            ps.setInt(4, appointment.getStatus());
+            ps.setInt(5, appointment.getAppointId());
+            int n = ps.executeUpdate();
+//            System.out.println(n);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void requestAppointment(Appointment appointment) {
+        String query = "insert into Appointment values (?,?,?,?,?)"; //patientId, docId, time, note, status
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, appointment.getPatientId());
+            ps.setInt(2, appointment.getDoctorId());
+            ps.setTimestamp(3, new Timestamp(appointment.getTime().getTime()));
+            ps.setString(4, appointment.getNote());
+            ps.setInt(5, appointment.getStatus());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+}
--- src/main/webapp/view/appoint/AppointList.jsp
+++ src/main/webapp/view/appoint/AppointList.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -25,21 +26,21 @@
                             <h2 class="card-title text-uppercase"><strong>All Appointment</strong></h2>
                         </div>
                         <div class="card-body">
-                            <form action="class" method="get">
+                            <form action="appointment" method="get">
                                 <div class="row">
-                                    <select class="form-control selectpicker col-2" name="search_status"
+                                    <select class="form-control selectpicker col-2" name="searchStatus"
                                             onchange="this.form.submit()" style="height: 40px;margin-right: 3px;">
-                                        <option value="">All Role</option>
+                                        <option value="">All Status</option>
                                         <jsp:useBean id="c" class="com.example.swd392_clinic_management.util.Config"/>
                                         <c:set var="listStatus" value="${c.listAppointStatus}"/>
                                         <c:forEach items="${listStatus}" var="s" begin="0" varStatus="i">
-                                            <option value="${i.count}">${s}</option>
+                                            <option value="${i.count - 1}" ${searchStatus == i.count - 1 ? "selected":""}>${s}</option>
                                         </c:forEach>
                                     </select>
                                     <input type="text" style="margin-right: 3px;"
-                                           value="${requestScope.search_value==null?"":requestScope.search_value}"
-                                           size="30%" placeholder="Type ... to search"
-                                           name="search_value"/>
+                                           value="${searchKey==null?"":searchKey}"
+                                           size="30%" placeholder="Search by doctor or patient's name..."
+                                           name="searchKey"/>
                                     <button style="margin-right: 3px;" type="submit" class="btn btn-primary">Search
                                     </button>
 
@@ -47,32 +48,32 @@
                             </form>
                             <br/>
                             <table class="table table-bordered table-hover">
-                                    <thead>
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Doctor</th>
+                                    <th>Patient</th>
+                                    <th>Time</th>
+                                    <th>Status</th>
+                                    <th>Detail</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <c:forEach items="${list}" var="l">
                                     <tr>
-                                        <th>ID</th>
-                                        <th>Doctor</th>
-                                        <th>Patient</th>
-                                        <th>Time</th>
-                                        <th>Status</th>
-                                        <th>Detail</th>
+                                        <td>${l.appointId}</td>
+                                        <td>${l.doctorName}</td>
+                                        <td>${l.patientName}</td>
+                                        <td><fmt:formatDate value="${l.time}" pattern="HH:mm dd-MM-yyyy"/></td>
+                                        <td>${listStatus[l.status]}</td>
+                                        <td>
+                                            <a class="btn btn-info btn-sm" style="margin-right:10%;"
+                                               href="appointment?tag=update&id=${l.appointId}"><i
+                                                    class="fas fa-pencil-alt"></i></a>
+                                        </td>
                                     </tr>
-                                    </thead>
-                                    <tbody>
-                                    <c:forEach items="${requestScope.listPerPage}" var="l">
-                                        <tr>
-                                            <td>${l.id}</td>
-                                            <td>${l.doctor}</td>
-                                            <td>${l.patient}</td>
-                                            <td>${l.time}</td>
-                                            <td>${l.status}</td>
-                                            <td>
-                                                <a class="btn btn-info btn-sm" style="margin-right:10%;"
-                                                   href="class?tag=update&id=${l.classId}"><i
-                                                        class="fas fa-pencil-alt"></i></a>
-                                            </td>
-                                        </tr>
-                                    </c:forEach>
-                                    </tbody>
+                                </c:forEach>
+                                </tbody>
                             </table>
                         </div>
                     </div>
@@ -80,6 +81,42 @@
             </div>
         </div>
     </section>
+    <c:set var="curPage" value="${requestScope.curPage}"/>
+    <c:set var="NoPage" value="${requestScope.num}"/>
+    <c:if test="${! empty requestScope.list}">
+        <nav aria-label="..." id="paging">
+            <ul class="pagination d-flex justify-content-center">
+                <li class="page-item disabled">
+                    <c:if test="${curPage!=1}">
+                <li class="page-item">
+                    <a class="page-link"
+                       href="appointment?curPage=${1}&searchKey=${searchKey}&searchStatus=${searchStatus}">Begin</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link"
+                       href="appointment?curPage=${curPage - 1}&searchKey=${searchKey}&searchStatus=${searchStatus}">Previous</a>
+                </li>
+                </c:if>
+                <c:forEach begin="${curPage>=2?curPage-1:curPage}" end="${curPage+2>NoPage?NoPage:curPage+2}" var="i">
+                    <li class="page-item ${i==curPage?"active":""}">
+                        <a class="page-link"
+                           href="appointment?curPage=${i}&searchKey=${searchKey}&searchStatus=${searchStatus}">${i}</a>
+                    </li>
+                </c:forEach>
+                <c:if test="${curPage!=NoPage}">
+                    <li class="page-item">
+                        <a class="page-link"
+                           href="appointment?curPage=${curPage + 1}&searchKey=${searchKey}&searchStatus=${searchStatus}">Next</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link"
+                           href="appointment?curPage=${NoPage}&searchKey=${searchKey}&searchStatus=${searchStatus}">End</a>
+                    </li>
+                </c:if>
+                </li>
+            </ul>
+        </nav>
+    </c:if>
 </div>
 
 <%@include file="../home/Footer.jsp" %>
